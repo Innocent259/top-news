e230b36b8c44f186ce74f1b77367c816ade29bfa
Innocent NDAHIMANA <ndahimanainnocent842@gmail.com>
2023-08-03 08:02:59 +0200

    Display Navigation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Navigation from './components/Navigation';
 import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
+      <Navigation />
       <Home />
     </div>
   );
