1192a16ef2993a59500bd31e57e6e010640a746c
Innocent NDAHIMANA <ndahimanainnocent842@gmail.com>
2023-08-03 07:12:05 +0200

    Create redux store

---
+++ src/redux/store.js
@@ -0,0 +1,9 @@
+import { configureStore } from "@reduxjs/toolkit";
+import newsReducer from "./news/newsSlice";
+
+const store = configureStore({
+    reducer: {
+        news: newsReducer
+    }
+})
+export default store;
