a96f1a5833d336ab6a7d5ea5685da3e7fc96e54c
Innocent NDAHIMANA <ndahimanainnocent842@gmail.com>
2023-08-04 20:52:06 +0200

    Create a new component for more details

--- src/components/FullDetails.js
+++ src/components/FullDetails.js
@@ -1,17 +1,30 @@
-import React from 'react';
+import React from "react";
+import { useLocation } from "react-router-dom";
 
-const FullDetails = ({ newsData, pillarName }) => {
-  const filteredNews = newsData.filter((item) => item.pillarName === pillarName);
+const FullDetails = () => {
+  const location = useLocation();
+  const data = location.state;
 
   return (
-    <ul>
-      {filteredNews.map((item) => (
-        <li key={item.id}>
-          <div>{item.type}</div>
-          <h2>{item.webTitle}</h2>
-        </li>
-      ))}
-    </ul>
+    <div className="full-details-data py-5 text-start d-flex flex-column">
+      <h3 className="display-5 p-3 flex-fill item-1">
+        News section name: {data.sectionName}
+      </h3>
+      <p className="display-5 p-3 flex-fill item-2">
+        Publication date: {data.webPublicationDate}
+      </p>
+      <p className="display-6 p-3 flex-fill item-3">
+        Web title: {data.webTitle}
+      </p>
+      <h3 className="display-5 p-3 flex-fill item-4">
+        New pillar name: {data.pillarName}
+      </h3>
+      <span className="display-5 bg-info p-3 text-center flex-fill item-5">
+        <a href={data.webUrl} className="btn w-75">
+          Read the news
+        </a>
+      </span>
+    </div>
   );
 };
 
