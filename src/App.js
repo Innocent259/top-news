5e62c3354c5c6ee896951e37948218b0c9aa95a4
Innocent NDAHIMANA <ndahimanainnocent842@gmail.com>
2023-08-04 20:50:20 +0200

    Use React router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import './App.css';
-import Navigation from './components/Navigation';
-import Home from './components/Home';
+import "./App.css";
+import Navigation from "./components/Navigation";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Introduction from "./components/Introduction";
+import Home from "./components/Home";
+import FullDetails from "./components/FullDetails";
 
 function App() {
   return (
-    <div className="App">
-      <Navigation />
-      <Home />
+    <div className="app h-100">
+      <BrowserRouter>
+        <Navigation />
+        <Introduction />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/:id" element={<FullDetails />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
